722c8e65db33abfd2d9d68137fb1f29863c1f69e
Ted Tibbetts <intuited@gmail.com>
2010-06-21 15:16:31 -0300

    add doctests to the `iterlog` function.
    v All tests pass.

--- __init__.py
+++ __init__.py
@@ -1,8 +1,24 @@
 """Various iterable-related boons."""
 
 def iterlog(it, stringifier=str, **kwargs):
+    """Log iterable elements as they pass through this node.
+    
+    >>> import logging
+    >>> import sys
+    >>> loghandler = logging.StreamHandler(sys.stdout)
+    >>> logger = logging.getLogger()
+    >>> logger.addHandler(loghandler)
+    >>> noisemaker = ('noise: {0}'.format(i) for i in range(2))
+    >>> noiselogger = iterlog(noisemaker)
+    >>> noise_fan = ('I like {0}'.format(i) for i in noiselogger)
+    >>> print noise_fan.next()
+    noise: 0
+    I like noise: 0
+    >>> print noise_fan.next()
+    noise: 1
+    I like noise: 1
+    """
     from logging import warning
     for i in it:
-        """Log iterable elements as they pass through this node."""
         warning(stringifier(i), **kwargs)
         yield i
